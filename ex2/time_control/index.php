09fd15aa548de4e33fe98e0516c65f5dcb7c49e2
alentev <Alentev.A@globalsolutions.ru>
2021-05-29 13:33:19 +0300

    2-11

---
+++ ex2/time_control/index.php
@@ -0,0 +1,13 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+
+    <p>
+        Самая ресурсоёмкая страница - /products/index.php, время выполнения - 0.73 с
+    </p>
+    <p>
+        Компонент с наибольшим количеством запросов - bitrix:catalog, запросов: 17.
+    </p>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
