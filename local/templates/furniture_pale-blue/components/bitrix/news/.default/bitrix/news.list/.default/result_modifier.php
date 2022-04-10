fa7b40a63ff3bc1c0389bb0f077ed1dcaa6d0c3d
alentev <Alentev.A@globalsolutions.ru>
2022-04-10 12:20:25 +0300

    new comparsion operator

--- local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
+++ local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -1,6 +1,6 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 if ($arParams['SET_SPECIALDATE_PROPERTY'] == 'Y'){
-    $firstNewsDate = $arResult['ITEMS'][0]['DISPLAY_ACTIVE_FROM'] ? $arResult['ITEMS'][0]['DISPLAY_ACTIVE_FROM'] : false;
+    $firstNewsDate = $arResult['ITEMS'][0]['DISPLAY_ACTIVE_FROM'] ?? false;
     $arResult['FIRST_NEWS_DATE'] = $firstNewsDate;
     $ob_componenet = $this->GetComponent();
     $ob_componenet->SetResultCacheKeys(array('FIRST_NEWS_DATE'));
